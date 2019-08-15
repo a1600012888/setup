782f9468caa7e0dfc6e43c8d0d5f3b89a98debd1
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-08-15 02:28:41 +0000

    adding scripts for install maskrcnn_benchmakr

---
+++ mask.sh
@@ -0,0 +1,26 @@
+cd /rscratch/tyz/
+conda create --name maskrcnn_benchmark
+conda activate maskrcnn_benchmark
+conda install ipython
+pip install ninja yacs cython matplotlib tqdm opencv-python --user
+pip install protobuf pillow --user
+
+export INSTALL_DIR=$PWD
+cd cocoapi/PythonAPI
+rm -rf build/
+python setup.py build_ext install
+
+cd $INSTALL_DIR
+cd apex
+rm -rf build/
+python setup.py install --cuda_ext --cpp_ex
+
+cd $INSTALL_DIR
+# git clone https://github.com/facebookresearch/maskrcnn-benchmark.git
+cd maskrcnn-benchmark
+
+rm -rf build/
+python setup.py build develop
+
+
+unset INSTALL_DIR
